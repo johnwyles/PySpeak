6f6fa4d047fee1e0c9d9180078305d6f1a2130f0
John Wyles <johnw@zynga.com>
2013-04-22 20:56:33 -0500

    Updating with commandline parameters, startup logic, and logging

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,17 +1,21 @@
+import argparse
+import audioop
+import logging
+import os
 import pyaudio
 import wave
-import audioop
-from collections import deque 
-import os
-import urllib2
-import urllib
 import time
-
+import urllib
+import urllib2
 from pprint import pprint
+from collections import deque
+
 
 class Listener():
+    LANGUAGE = 'en-US'
+    MAX_RESULTS = 6
+
     def __init__(self, destination='file', silence_limit=2, threshold=20, bits=pyaudio.paInt16, channels=1, rate=16000, chunk=1024):
-        #where to store the file on stop
         self.destination = destination
         self.all_chunks = []
         self.bits = bits
@@ -23,17 +27,18 @@ class Listener():
         self.recorded_chunks = 0
 
     def start(self):
-        print "* listening. CTRL+C to finish."
+        logging.info('Listening for speech input')
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format = self.bits,
               channels = self.channels,
               rate = self.rate,
               input = True,
               frames_per_buffer = self.chunk)
-        print "* recording"
+
         rel = self.rate/self.chunk
         sliding_window = deque(maxlen=self.silence_limit*rel)
         started = False
+
         while (True):
             try:
                 data = self.stream.read(self.chunk)
@@ -45,11 +50,11 @@ class Listener():
 
             if(True in [ x>self.threshold for x in sliding_window]):
                 if(not started):
-                    print "...starting record"
+                    logging.info('Speech input detected.  Recording raw audio')
                 started = True
                 self.all_chunks.append(data)
             elif(started == True):
-                print "...finished"
+                logging.info('Speech input no longer detected')
                 data = ''.join(self.all_chunks)
                 wf = wave.open(self.destination + '.wav', 'wb')
                 wf.setnchannels(self.channels)
@@ -61,18 +66,17 @@ class Listener():
                 started = False
                 sliding_window = deque(maxlen=self.silence_limit*rel)
                 self.all_chunks = []
-                print "...listening"
 
-        print "* done recording"
+        print logging.info('Completed listening for speech input')
         self.stop()
 
 
     def stop(self):
-        print "* done recording"
+        print logging.info('Stopping speech input detection')
         self.stream.close()
         self.p.terminate()
 
-        print "* saving"
+        print logging.info('Writing out speech input')
         # write data to WAVE file
         data = ''.join(self.all_chunks)
         wf = wave.open(self.destination, 'wb')
@@ -86,23 +90,49 @@ class Listener():
 
     def stt_google_wav(self):
         #Convert to flac
+        logging.info('Opening speech input WAV file and converting to FLAC')
         os.system("flac -f " + self.destination + '.wav')
-        f = open(self.destination + '.flac','rb')
-        flac_cont = f.read()
-        f.close()
+        file_handle = open(self.destination + '.flac','rb')
+        flac_cont = file_handle.read()
+        file_handle.close()
 
-        #post it
-        lang_code='en-US'
-        googl_speech_url = 'https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&pfilter=2&lang=%s&maxresults=6'%(lang_code)
-        hrs = {"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",'Content-type': 'audio/x-flac; rate=44100'}
-        req = urllib2.Request(googl_speech_url, data=flac_cont, headers=hrs)
-        p = urllib2.urlopen(req)
-
-        res = eval(p.read())['hypotheses']
-        print res
+        logging.info('Sending FLAC speech input to Google for interpretation')
+        google_speech_url = 'https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&pfilter=2&lang=%s&maxresults=%s'%(self.LANGUAGE, self.MAX_RESULTS)
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",'Content-type': 'audio/x-flac; rate=%s'%(self.rate)}
+        request = urllib2.Request(google_speech_url, data=flac_cont, headers=headers)
+        url_handle = urllib2.urlopen(request)
+        result = eval(url_handle.read())['hypotheses']
+        logging.info('Got response from Google: ' + result)
+        logging.info('Removing speech input WAV and FLAC files')
         map(os.remove, ([self.destination + '.wav', self.destination + '.flac']))
-        return res
+        return result
 
 
-l = Listener()
-l.start()
+if __name__ == '__main__':
+    APP_NAME = 'pyspeak'
+    APP_VERSION = '0.0.1'
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--loglevel', default='INFO', help='Log level for console output (DEBUG, INFO, WARNING, ERROR, CRITICAL')
+    parser.add_argument('-v', '--version', help='Get the version number and exit', const='version', nargs='?')
+    arguments = parser.parse_args()
+
+    # Print the version number and exit
+    if arguments.version:
+        print APP_NAME + ": " + APP_VERSION
+        exit()
+
+    # Setup logging
+    try:
+        number_level = getattr(logging, arguments.loglevel.upper(), None)
+        if not isinstance(number_level, int):
+            raise ValueError('Invalid log level: %s' % number_level)
+    except:
+        number_level
+
+    logging.basicConfig(format='[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s', level=number_level, nargs='?')
+
+    # Setup the speech listener
+    listener = Listener()
+    listener.start()
