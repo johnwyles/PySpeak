ac9c7f08929b18119ee0c9aa1138964efe039a85
John Wyles <johnw@zynga.com>
2013-05-01 23:50:52 -0500

    fast_commit through Fabric

--- pyspeak/commandline.py
+++ pyspeak/commandline.py
@@ -1,5 +1,12 @@
 """
-  Commandline
+Commandline execute function
+
+Example (already included in 'pyspeak' executable in this project):
+    #!/usr/bin/evn python
+
+    from pyspeak.commandline import execute
+    execute()
+
 """
 
 import argparse
@@ -10,15 +17,24 @@ from listener import Listener
 
 
 def execute():
+    """
+    Execute method called from the commandline executeable
+    """
+
     # Parse command line arguments
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     subparsers = parser.add_subparsers(help='Subcommand for pyspeak to either listen for microphone input (speech-to-text) or text input (text-to-speech)')
+
+    # Setup Listener argument parser subcommand
     parser_listener = subparsers.add_parser('listen', help='Listen for microphone input (speech-to-text)')
     parser_listener.add_argument('-f', '--filename-prefix', default='pyspeak_file', help='Default prefix location and filename for the temporary file %(prog) uses to store data.  This stores a .wav and .flac file of this prefix (e.g. "./pyspeak_file" => [./pyspeak_file.wav, ./pyspeak_file.flac])')
     parser_listener.add_argument('-s', '--silence-time', type=int, default=2, help='Amount of silence time (in seconds) to listen for before dispatching voice data for recognition')
-    parser_listener.add_argument('-t', '--threshold', type=int, default=60, help='Threshold for detecting speech input; depending on your microphone settings you may need to experiment a bit with this value')
+    parser_listener.add_argument('-t', '--threshold', type=int, default=80, help='Threshold for detecting speech input; depending on your microphone settings you may need to experiment a bit with this value')
+    parser_listener.set_defaults(func=_listener)
 
+    # Setup Speaker argument parser subcommand
     parser_speaker = subparsers.add_parser('speak', help='Listen for text input (text-to-speech')
+    parser_speaker.set_defaults(func=_speaker)
 
     parser.add_argument('-l', '--loglevel', default='INFO', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Log level for console output')
     parser.add_argument('-v', '--version', help='Get the version number and exit', action='store_true')
@@ -36,8 +52,23 @@ def execute():
             raise ValueError('Invalid log level: %s' % number_level)
     except:
         number_level
-
     logging.basicConfig(format='[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s', level=number_level, nargs='?')
 
+    # Callback to argument parser subcommands
+    arguments.func(arguments)
+
+
+def _listener(arguments):
+    """
+    Listener subcommand callback
+    """
+    logging.info('Starting Listener')
     listener = Listener(silence_time=arguments.silence_time, threshold=arguments.threshold, filename_prefix=arguments.filename_prefix)
-    listener.start()
+    listener.get_utterance()
+
+
+def _speaker(arguments):
+    """
+    Speaker subcommand callback
+    """
+    logging.info('Starting Speaker')
--- pyspeak/listener.py
+++ pyspeak/listener.py
@@ -7,18 +7,31 @@ import urllib
 import urllib2
 import wave
 import collections
-# from pprint import pprint
+from pprint import pprint
 
 
 class Listener():
-    def __init__(self, filename_prefix='pyspeak_file', silence_time=2, threshold=60, language='en-US', max_results=4, bits=pyaudio.paInt16, channels=1, rate=44100, chunk=1024):
-        logging.info('PySpeak listener initializing')
+    """
+    Listener class
+
+    TODO(): put more infoation here
+    """
+    def __init__(self, filename_prefix='pyspeak_file', silence_time=2, threshold=80, language='en-US', max_results=4, bits=pyaudio.paInt16, channels=1, rate=44100, chunk=2048):
+        """
+        Initializer
+
+        TODO(): Put more information here
+        """
+        logging.info('Listener is initializing')
+        
+        # Make sure the format is initialized
         self.pyaudio_handler = pyaudio.PyAudio()
         device_input_index = self._find_input_device_index()
         if not self.pyaudio_handler.is_format_supported(input_device=device_input_index, rate=rate, input_channels=channels, input_format=bits):
             logging.error('Unsupported format given while initializing')
             exit(1)
 
+        # Instance variables
         self._all_chunks = []
         self.bits = bits
         self.channels = channels
@@ -31,7 +44,23 @@ class Listener():
         self.threshold = threshold
 
 
-    def start(self):
+    def __del__(self):
+        """
+        Destructor to teardown the pyaudio instance we created
+        """
+        self.pyaudio_handler.terminate()
+
+
+    def get_utterance(self):
+        """
+        TODO(): Put a ton of information here
+        """
+        # Do some initialization
+        window_size = self.silence_time*(self.rate/self.chunk)
+        sliding_window = collections.deque(maxlen=window_size)
+        utterance = None
+
+        # Setup the pyaudio stream
         logging.info('Listening for speech input')
         self.stream = self.pyaudio_handler.open(format=self.bits,
             channels=self.channels,
@@ -39,43 +68,46 @@ class Listener():
             input=True,
             frames_per_buffer=self.chunk)
 
-        rel = self.rate/self.chunk
-        sliding_window = collections.deque(maxlen=self.silence_time*rel)
+        # Start recording some data
         started = False
-
-        while (True):
+        recording = True
+        while (recording):
+            # Get a chunk of data from the stream
             try:
                 data = self.stream.read(self.chunk)
             except IOError as e:
                 data = '\x00' * self.chunk
                 logging.warning('Probably just a hiccup in the recording: ' + str(e))
 
+            # Get average of the last two bytes in the window and keep track of it
             sliding_window.append(abs(audioop.avg(data, 2)))
 
+            # If the average of the chunk exceeds the threshold keep the data
             if(True in [x>self.threshold for x in sliding_window]):
                 if(not started):
                     logging.info('Speech input detected.  Recording raw audio')
                 started = True
                 self._all_chunks.append(data)
-            elif(started == True):
+
+            # If the average of the chunk is
+            elif(started):
                 logging.info('Speech input no longer detected')
+                recording = False
                 self._write()
-                self._get_google_transciption()
-                started = False
-                sliding_window = collections.deque(maxlen=self.silence_time*rel)
+                utterance = self._get_google_transciption()
+                sliding_window = collections.deque(maxlen=window_size)
                 self._all_chunks = []
+                self.stream.close()
 
-        logging.info('Completed listening for speech input')
-        self.stop()
-
-
-    def stop(self):
-        logging.info('Stopping speech input detection')
-        self.stream.close()
-        self.pyaudio_handler.terminate()
+        return utterance
 
 
     def _find_input_device_index(self):
+        """
+        Find the audio input device by looking through the known devices
+
+        TODO(): Put information about the return
+        """
         device_index = None            
         for i in range( self.pyaudio_handler.get_device_count() ):     
             devinfo = self.pyaudio_handler.get_device_info_by_index(i)   
@@ -94,6 +126,9 @@ class Listener():
 
 
     def _write(self):
+        """
+        Helper method write out the recorded contents
+        """
         logging.info('Writing out speech input')
         data = ''.join(self._all_chunks)
         wav_file = wave.open(self.filename_prefix + '.wav', 'wb')
@@ -109,6 +144,11 @@ class Listener():
 
 
     def _get_google_transciption(self):
+        """
+        Call the Google speech API to get the transcription for the FLAC encoded data
+
+        TODO(): Put information about the return
+        """
         logging.info('Getting FLAC file contents')
         file_handle = open(self.filename_prefix + '.flac','rb')
         flac_cont = file_handle.read()
@@ -119,8 +159,11 @@ class Listener():
         headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",'Content-type': 'audio/x-flac; rate=%s' % (self.rate)}
         request = urllib2.Request(google_speech_url, data=flac_cont, headers=headers)
         url_handle = urllib2.urlopen(request)
+
         result = eval(url_handle.read())['hypotheses']
         logging.info('Got response from Google: ' + str(result))
+
         logging.info('Removing speech input WAV and FLAC files')
         map(os.remove, ([self.filename_prefix + '.wav', self.filename_prefix + '.flac']))
+
         return result
